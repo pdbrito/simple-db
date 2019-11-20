ee21a5cdb76446da9dd5f5e948f22925bb0806e6
Pedro de Brito <ped.de.brito@gmail.com>
2019-11-20 18:30:03 +0100

    Update main()
    If the input starts with . handle it with do_meta_command()

--- simple_db.c
+++ simple_db.c
@@ -65,11 +65,14 @@ int main(int argc, char *argv[]) {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        if (input_buffer->buffer[0] == '.') {
+           switch (do_meta_command(input_buffer)) {
+               case META_COMMAND_SUCCESS:
+                   continue;
+               case META_COMMAND_UNRECOGNIZED_COMMAND:
+                   printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+                   continue;
+           }
         }
     }
 }
