9e2f950f80f5fad2c8eb02b98dc3948177ab590b
Pedro de Brito <ped.de.brito@gmail.com>
2019-11-20 18:26:05 +0100

    Update do_meta_command()
    Call close_input_buffer() before exiting

--- simple_db.c
+++ simple_db.c
@@ -51,6 +51,7 @@ typedef enum {
 
 MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        close_input_buffer(input_buffer);
         exit(EXIT_SUCCESS);
     } else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
