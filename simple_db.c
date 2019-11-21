f00e1890173faa69eb2dc237948f0270f5b74e57
Pedro de Brito <ped.de.brito@gmail.com>
2019-11-21 22:56:27 +0100

    Add prepare_statement()

--- simple_db.c
+++ simple_db.c
@@ -49,6 +49,27 @@ typedef enum {
     PREPARE_UNRECOGNIZED_COMMAND
 } PrepareResult;
 
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_COMMAND;
+}
+
 MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
         close_input_buffer(input_buffer);
