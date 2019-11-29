90a52d0b4c4ce989dc7580c5ac21fbcf5e1fee68
Pedro de Brito <ped.de.brito@gmail.com>
2019-11-29 22:35:18 +0100

    Declare Table struct

--- simple_db.c
+++ simple_db.c
@@ -34,6 +34,16 @@ void deserialize_row(void *source, Row *destination) {
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+    uint32_t num_rows;
+    void *pages[TABLE_MAX_PAGES];
+} Table;
+
 typedef struct {
     char *buffer;
     size_t buffer_length;
