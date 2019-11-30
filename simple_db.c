e70adbec80dbffd91761ad2397445fb269d7dc9a
Pedro de Brito <ped.de.brito@gmail.com>
2019-11-30 14:28:49 +0100

    Add row_slot() function
    Returns a pointer to read from/write to

--- simple_db.c
+++ simple_db.c
@@ -44,6 +44,17 @@ typedef struct {
     void *pages[TABLE_MAX_PAGES];
 } Table;
 
+void *row_slot(Table *table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void *page = table->pages[page_num];
+    if (page == NULL) {
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
 typedef struct {
     char *buffer;
     size_t buffer_length;
