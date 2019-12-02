bf9d1a83979165387bd2557d73d8782b1dcfb07e
Pedro de Brito <ped.de.brito@gmail.com>
2019-12-02 22:02:03 +0100

    Add execute_insert()

--- simple_db.c
+++ simple_db.c
@@ -139,6 +139,19 @@ typedef enum {
     EXECUTE_TABLE_FULL
 } ExecuteResult;
 
+ExecuteResult execute_insert(Statement *statement, Table *table) {
+    if (table->num_rows >= TABLE_MAX_ROWS) {
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row *row_to_insert = &(statement->row_to_insert);
+
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+
+    return EXECUTE_SUCCESS;
+}
+
     switch (statement->type) {
         case (STATEMENT_INSERT):
             printf("This is where we would do an insert. \n");
