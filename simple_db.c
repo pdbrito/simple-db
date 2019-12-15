3c52d019aa94bae493a2ad2f61a17dca93cd2b3b
Pedro de Brito <ped.de.brito@gmail.com>
2019-12-15 12:16:20 +0100

    Remove unnecessary prototype

--- simple_db.c
+++ simple_db.c
@@ -42,8 +42,6 @@ const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
-void *get_page(Pager *pager, uint32_t num);
-
 typedef struct {
     int file_descriptor;
     uint32_t file_length;
