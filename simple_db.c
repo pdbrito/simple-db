142eb582fb2c17a89272a3d3f3fa0625c261dc73
Pedro de Brito <ped.de.brito@gmail.com>
2019-12-18 14:57:59 +0100

    Refactor row_slot() into cursor_value()
    Returns a pointer to the position currently described by the cursor

--- simple_db.c
+++ simple_db.c
@@ -108,9 +108,10 @@ Cursor *table_end(Table *table) {
     return cursor;
 }
 
-void *row_slot(Table *table, uint32_t row_num) {
+void *cursor_value(Cursor *cursor) {
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void *page = get_page(table->pager, page_num);
+    void *page = get_page(cursor->table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
     return page + byte_offset;
