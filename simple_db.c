89ffed396be16d63f8c68abd21524ede39b1170f
Pedro de Brito <ped.de.brito@gmail.com>
2019-12-20 18:20:35 +0000

    Update execute_select to use cursor

--- simple_db.c
+++ simple_db.c
@@ -252,11 +252,16 @@ void print_row(Row *row) {
 }
 
 ExecuteResult execute_select(Statement *statement, Table *table) {
+    Cursor *cursor = table_start(table);
     Row row;
-    for (uint32_t i = 0; i < table->num_rows; i++) {
-        deserialize_row(row_slot(table, i), &row);
+    while (!(cursor->end_of_table)) {
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+
+    free(cursor);
+
     return EXECUTE_SUCCESS;
 }
 
