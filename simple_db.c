bbd5c670c41b512a6a5b6407b273e684d560c875
Pedro de Brito <ped.de.brito@gmail.com>
2019-12-31 20:18:56 +0000

    Update execute_insert()
    Since the tree only has one node use leaf_node_insert() to add rows

--- simple_db.c
+++ simple_db.c
@@ -321,15 +321,15 @@ void leaf_node_insert(Cursor *cursor, uint32_t key, Row *value) {
 }
 
 ExecuteResult execute_insert(Statement *statement, Table *table) {
-    if (table->num_rows >= TABLE_MAX_ROWS) {
+    void *node = get_page(table->pager, table->root_page_num);
+    if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS)) {
         return EXECUTE_TABLE_FULL;
     }
 
     Row *row_to_insert = &(statement->row_to_insert);
     Cursor *cursor = table_end(table);
 
-    serialize_row(row_to_insert, cursor_value(cursor));
-    table->num_rows += 1;
+    leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
 
     free(cursor);
 
