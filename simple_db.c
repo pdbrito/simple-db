3d580ba79675fc5aab1e83ce105550485bfd57f5
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-03 18:21:13 +0100

    Add EXECUTE_DUPLICATE_KEY value to ExecuteResult enum

--- simple_db.c
+++ simple_db.c
@@ -294,6 +294,7 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
 
 typedef enum {
     EXECUTE_SUCCESS,
+    EXECUTE_DUPLICATE_KEY,
     EXECUTE_TABLE_FULL
 } ExecuteResult;
 
