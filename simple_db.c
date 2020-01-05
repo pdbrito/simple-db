671167b080773ac1e29831d617ca1331f88f23d4
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-05 21:51:49 +0100

    Remove error handling for a full leaf node

--- simple_db.c
+++ simple_db.c
@@ -350,8 +350,8 @@ void leaf_node_insert(Cursor *cursor, uint32_t key, Row *value) {
     uint32_t num_cells = *leaf_node_num_cells(node);
     if (num_cells >= LEAF_NODE_MAX_CELLS) {
         // Node full
-        printf("Need to implement splitting a leaf node. \n");
-        exit(EXIT_FAILURE);
+        leaf_node_split_and_insert(cursor, key, value);
+        return;
     }
 
     if (cursor->cell_num < num_cells) {
@@ -370,9 +370,6 @@ void leaf_node_insert(Cursor *cursor, uint32_t key, Row *value) {
 ExecuteResult execute_insert(Statement *statement, Table *table) {
     void *node = get_page(table->pager, table->root_page_num);
     uint32_t num_cells = (*leaf_node_num_cells(node));
-    if ((num_cells >= LEAF_NODE_MAX_CELLS)) {
-        return EXECUTE_TABLE_FULL;
-    }
 
     Row *row_to_insert = &(statement->row_to_insert);
     uint32_t key_to_insert = row_to_insert->id;
